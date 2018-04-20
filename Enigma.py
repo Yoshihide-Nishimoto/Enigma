c18835f05773fbd4b8b2556ebe57b8687daaae67
Yoshihide-Nishimoto <ynishimoto@wbcompany.co.jp>
2018-04-20 15:56:02 +0900

    update

--- Enigma.py
+++ Enigma.py
@@ -2,34 +2,34 @@ import argparse
 import string
 ALPHABETS = string.ascii_uppercase
 
-def enigma(wList1,wList2,wList3,pList,count,inp):
+def enigma(rotor1,rotor2,rotor3,reflector,count,inp):
 
-    cLists = convertLists(wList1,wList2,wList3,count)
-    cList1 = cLists[0]
-    cList2 = cLists[1]
-    cList3 = cLists[2]
+    cRotors = convertRotors(rotor1,rotor2,rotor3,count)
+    cRotor1 = cRotors[0]
+    cRotor2 = cRotors[1]
+    cRotor3 = cRotors[2]
 
-    p1 = cList1[inp]
-    p2 = cList2[p1]
-    p3 = cList3[p2]
-    p4 = pList[p3]
-    p5 = cList3.index(p4)
-    p6 = cList2.index(p5)
-    out = cList1.index(p6)
+    p1 = cRotor1[inp]
+    p2 = cRotor2[p1]
+    p3 = cRotor3[p2]
+    p4 = reflector[p3]
+    p5 = cRotor3.index(p4)
+    p6 = cRotor2.index(p5)
+    out = cRotor1.index(p6)
     return out
 
-def convertLists(wList1,wList2,wList3,count):
-    wList1Shift = count % len(wList1)
-    wList2Shift = (count / len(wList2)) % len(wList2)
-    wList3Shift = (count / (len(wList3) ** 2)) % len(wList3)
+def convertRotors(rotor1,rotor2,rotor3,count):
+    rotor1Shift = count % len(rotor1)
+    rotor2Shift = (count / len(rotor2)) % len(rotor2)
+    rotor3Shift = (count / (len(rotor3) ** 2)) % len(rotor3)
 
-    clist1 = convertList(wList1,wList1Shift)
-    clist2 = convertList(wList2,wList2Shift)
-    clist3 = convertList(wList3,wList3Shift)
+    cRotor1 = convertRotor(rotor1,rotor1Shift)
+    cRotor2 = convertRotor(rotor2,rotor2Shift)
+    cRotor3 = convertRotor(rotor3,rotor3Shift)
 
-    return clist1,clist2,clist3
+    return cRotor1,cRotor2,cRotor3
 
-def convertList(list,rotation):
+def convertRotor(list,rotation):
     clist = [0] * len(list)
     count = 0
     for v in list:
@@ -47,7 +47,7 @@ def char_to_int(char):
 
 def int_to_char(int):
     c = ALPHABETS[int]
-    return c
+    return c.upper()
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-r1",
@@ -77,17 +77,16 @@ parser.add_argument("-inp",
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    wList1 = args.r1
-    wList2 = args.r2
-    wList3 = args.r3
-    pList  = args.ref
+    rotor1 = args.r1
+    rotor2 = args.r2
+    rotor3 = args.r3
+    reflector = args.ref
     inp  = args.inp
 
-count = 0
-
-for char in inp:
-    num  = char_to_int(char)
-    out  = enigma(wList1,wList2,wList3,pList,count,num)
-    rout = enigma(wList1,wList2,wList3,pList,count,out)
-    print('count: %s inp: %s out: %s rout: %s result: %s' % (count,char,int_to_char(out),int_to_char(rout),num==rout))
-    count += 1
+    count = 0
+    for char in inp:
+        num  = char_to_int(char)
+        out  = enigma(rotor1,rotor2,rotor3,reflector,count,num)
+        rout = enigma(rotor1,rotor2,rotor3,reflector,count,out)
+        print('count: %s inp: %s out: %s rout: %s result: %s' % (count,char,int_to_char(out),int_to_char(rout),num==rout))
+        count += 1
